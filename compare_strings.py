1766fc4551d35ac5814e37c101e647c50b50236e
Andrey Pavlov <yokinda@gmail.com>
2018-12-01 17:29:48 +0300

    elif лучше и читабельнее

--- compare_strings.py
+++ compare_strings.py
@@ -1,17 +1,17 @@
 def compare_str(first_str, second_str):
-    if not (type(first_str) == str and type(second_str) == str):
-        return 0
-    elif first_str == second_str:
-        return 1
-    else:
-        if len(first_str) > len(second_str):
-            return 2
-        if second_str == 'learn':
-            return 3
-        return 4
+	if not (type(first_str) == str and type(second_str) == str):
+		return 0
+	elif first_str == second_str:
+		return 1
+	elif len(first_str) > len(second_str):
+		return 2
+	elif second_str == 'learn':
+			return 3
+	else:
+		return 4
 
 result_dict = {0:'это не строки', 1:'одинаковые строки', 2:'первая строка длиннее',
-                 3:'вторая строка - learn', 4:'незаданные параметры задачи'}
+				 3:'вторая строка - learn', 4:'незаданные параметры задачи'}
 
 result = compare_str(123, True)
 print('{}: {}'.format(result, result_dict[result]))
